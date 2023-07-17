a726019dcbf51e434216dbf63111aa85e400cf18
Khader Vali <khader.kalava@telepo.com>
2023-07-17 21:16:45 +0200

    Init and final react demo project with stencil js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
+import { defineCustomElements } from '@telepo/dstny-ds/loader';
+import { DstnyButton } from '@telepo/dstny-ds/dist/dstny-ds-react';
 import './App.css';
 
+defineCustomElements();
+
 function App() {
+
+  const [ size, setSize ]  = useState('small');
+  const [ color, setColor ]  = useState('primary');
+
   return (
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" />  
+        <div className="btn-group">
+          <button type="button" onClick={() => setSize('small')}>Size sm</button>
+          <button type="button" onClick={() => setSize('large')}>Size L</button>
+          <button type="button" onClick={() => setSize('extra-large')}>Size XL</button>
+          <button type="button" onClick={() => setColor('primary')}>Color Primary</button>
+          <button type="button" onClick={() => setColor('secondary')}>Color Secondary</button>
+        </div>
+        <div className="dstny-base-theme">
+            <DstnyButton 
+              type="button"
+              size={size}
+              variant={color}
+              disabled='false'
+              label='Dstny Button CTA'
+            />
+        </div>
       </header>
     </div>
   );
